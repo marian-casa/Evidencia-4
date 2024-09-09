4eda06636ef4deb20fc4640edc268faa85fa0885
Mariano Casarino <mariano.e.casarino@gmail.com>
2024-09-08 23:40:44 -0300

    arreglos

--- endoscopio.py
+++ endoscopio.py
@@ -1,42 +1,46 @@
 
 class Endoscopio:
-
     def __init__(self):
-        self.esta_encendido = False
+        self._esta_encendido = False
         self._imagenes = []
         self._videos = []
-        self._max_video_duration = 60  #segundos
+        self._max_video_duration = 60  # en segundos, por ejemplo
     
     def encender(self):
-        self.esta_encendido= True
-        print('Endoscopio encendido.')
-
+        self._esta_encendido = True
+        print("Endoscopio encendido.")
     
     def apagar(self):
-        self.esta_encendido= False
-        print('Endoscopio apagado.')
-
+        self._esta_encendido = False
+        print("Endoscopio apagado.")
+    
     def capturar_imagen(self):
-        if self.esta_encendido:
-            imagen = f"imagen_{len(self._imagenes) +1}.jpg"
+        if self._esta_encendido:
+            imagen = f"imagen_{len(self._imagenes) + 1}.jpg"
             self._imagenes.append(imagen)
             print(f"{imagen} capturada.")
             return imagen
         else:
-            raise Exception('El endoscopio esta apagado, no puede capturar imagenes')
+            raise Exception("El endoscopio está apagado, no puede capturar imágenes.")
     
     def capturar_video(self, duracion):
-        if self.esta_encendido:
+        if self._esta_encendido:
             if duracion > self._max_video_duration:
-                raise ValueError(f'La duracion del video no puede exeder {self._max_video_duration} segundos.')
-            video = f'video_{len(self._videos) + 1}.mp4'
+                raise ValueError(f"La duración del video no puede exceder {self._max_video_duration} segundos.")
+            video = f"video_{len(self._videos) + 1}.mp4"
             self._videos.append(video)
-            print(f'{video} grabando durante {duracion} segundos.')
+            print(f"{video} grabado durante {duracion} segundos.")
             return video
         else:
-            raise Exception('El endoscopio esta apagado, no puede trasmitir video.')
-
+            raise Exception("El endoscopio está apagado, no puede grabar videos.")
+    
+    def transmitir_video(self):
+        if self._esta_encendido:
+            print("Transmitiendo video en tiempo real.")
+            return True
+        else:
+            raise Exception("El endoscopio está apagado, no puede transmitir video.")
     
     def __str__(self):
-        estado = "encendido" if self.esta_encendido else "apagado"
-        return f"El endoscopio esta actualmente {estado}"
+        estado = "encendido" if self._esta_encendido else "apagado"
+        return f"Endoscopio está actualmente {estado}."
--- test_endoscopio.py
+++ test_endoscopio.py
@@ -1,13 +1,14 @@
 import unittest
 from endoscopio import Endoscopio
 
+
 class TestEndoscopio(unittest.TestCase):
     
     def setUp(self):
         self.endoscopio = Endoscopio()
     
     def test_encender_apagar(self):
-        self.endoscopio.encender
+        self.endoscopio.encender()
         self.assertTrue(self.endoscopio._esta_encendido)
         self.endoscopio.apagar()
         self.assertFalse(self.endoscopio._esta_encendido)
@@ -15,30 +16,31 @@ class TestEndoscopio(unittest.TestCase):
     def test_captura_imagen_con_endoscopio_apagado(self):
         with self.assertRaises(Exception):
             self.endoscopio.capturar_imagen()
-
+    
     def test_captura_imagen_con_endoscopio_encendido(self):
         self.endoscopio.encender()
         imagen = self.endoscopio.capturar_imagen()
-        self.assertEqual(imagen, 'Imagen_1.jpg')
-
-    def test_trasmision_video_con_endoscopio_apagado(self):
-        with self.assertRaises(Exception):
-            self.endoscopio.trasmitir_video()
+        self.assertEqual(imagen, "imagen_1.jpg")
     
-    def test_trasmision_video(self):
+    def test_transmision_video_con_endoscopio_apagado(self):
+        with self.assertRaises(Exception):
+            self.endoscopio.transmitir_video()
+    
+    def test_transmision_video(self):
         self.endoscopio.encender()
-        self.assertTrue(self.endoscopio.trasmitir_video())
-
-    def test_captura_video(self):
+        self.assertTrue(self.endoscopio.transmitir_video())
+    
+    def test_capturar_video(self):
         self.endoscopio.encender()
         video = self.endoscopio.capturar_video(30)
         self.assertEqual(video, "video_1.mp4")
-
-    def test_captura_video_con_duracion_exesiva(self):
+    
+    def test_capturar_video_con_duracion_excesiva(self):
         self.endoscopio.encender()
         with self.assertRaises(ValueError):
             self.endoscopio.capturar_video(70)
 
 if __name__ == '__main__':
     unittest.main()
+
 
